100aabbaf96c298d46d10d598f9d6a6f83eba079
Yui Tokuhiro <y-tokuhiro@decoration-company.com>
2015-04-03 16:04:31 +0900

    Initial Commit

---
+++ GuruGuru/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuruGuru
+//
+//  Created by 徳弘佑衣 on 2015/04/03.
+//  Copyright (c) 2015年 徳弘佑衣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
