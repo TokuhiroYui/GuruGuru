9fb63a1acc40f004dfcbd6324236c3bf37e883c6
Yui Tokuhiro <y-tokuhiro@decoration-company.com>
2015-04-03 17:50:25 +0900

    gestureを追加

--- GuruGuru/ViewController.swift
+++ GuruGuru/ViewController.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        // ピンチ
+        let myPinchGesture = UIPinchGestureRecognizer(target: self, action: "pinchGesture:")
+        // パン
+        let myPan = UIPanGestureRecognizer(target: self, action: "panGesture:")
+        // 回転
+        let myRotate = UIRotationGestureRecognizer(target: self, action: "rotateGesture:")
+        
+        self.image.addGestureRecognizer(myPinchGesture)
+        self.image.addGestureRecognizer(myPan)
+        self.image.addGestureRecognizer(myRotate)
 
+    }
+    
+    //ピンチイベント(拡大・縮小)
+    func pinchGesture(sender: UIPinchGestureRecognizer){
+        //imageviewのsetAffineTransformにscaleを渡す
+        image.layer.setAffineTransform(CGAffineTransformMakeScale(sender.scale,sender.scale))
+    }
+    //パン(移動）
+    func panGesture(sender: UIPanGestureRecognizer){
+        //距離を取得
+        let p = sender.translationInView(self.view)
+        //移動した距離だけ、UIImageViewのcenterポジションを移動させる
+        let movedPoint = CGPointMake(image.center.x + p.x, image.center.y + p.y)
+        //imageに渡す
+        image.center = movedPoint
+        //[sender translationInView:]が返す距離は、ドラッグが始まってからの蓄積値となるため、
+        // 今回のようなドラッグに合わせてImageを動かしたい場合には、蓄積値をゼロにする
+        sender.setTranslation(CGPointZero, inView: self.view);
+    }
+    
+    //ローテート（回転）
+    func rotateGesture(sender: UIRotationGestureRecognizer){
+        image.layer.setAffineTransform(CGAffineTransformMakeRotation(sender.rotation))
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
